ef735814c7f6cdee0f142d5fa2df6acc6756913f
Gabriel Reyla <gabreyla@gmail.com>
2013-10-15 15:19:30 -0700

    good testing so far
    but I need to understand how the Meta attributes of
    django.db.models.Model is set in place, to understand what its default
    values are.

--- classes.py
+++ classes.py
@@ -1,29 +1,47 @@
 
+class OtherRandomClass(object):
+    def __str__(self):
+        return 'OtherRandomClass()'
+        
+class OtherRandomClassAbstract(object):
+    def __str__(self):
+        return 'OtherRandomClassAbstract()'
+    
+    class Meta:
+        abstract = True
+    pass
 
-class Model():
+class Model(object):
 
     def __str__(self):
         return 'Model()'
     
     class Meta:
-        abstract = True
+        abstract = False
     pass
 
-class Field():
+class Field(object):
+    def __init__(self, *args, **kwargs):
+        super(Field, self).__init__()
+
     def __str__(self):
         return 'Field()'
     
     class Meta:
-        abstract = True
+        abstract = False
     pass
 
 class CharField(Field):
+
+    def __init__(self, *args, **kwargs):
+        super(CharField, self).__init__(*args, **kwargs)
     
     def __str__(self):
         return 'CharField()'
     pass
 
 class SomeModel(Model):
+    field_name = CharField(max_length=100)
     
     def __str__(self):
         return 'SomeModel()'
@@ -35,3 +53,5 @@ if __name__ == '__main__':
     print Field()
     print CharField()
     print SomeModel()
+    print OtherRandomClass()
+    print OtherRandomClassAbstract()
--- meta_classes.py
+++ meta_classes.py
@@ -1,2 +1,27 @@
-import classes
+import inspect
 
+import classes
+from classes import Model
+
+def is_model(cls):
+    return issubclass(cls, Model)
+
+def is_abstract(cls):
+    """A model is abstract when it's Meta class has abstract==True"""
+    if hasattr(cls, 'Meta') and hasattr(cls.Meta, 'abstract'):
+        return cls.Meta.abstract
+    else:
+        return False
+
+def yield_module_classes(module):
+    for classes in dir(module):
+        if not classes.startswith('_'):
+            yield getattr(module, classes)
+
+if __name__ == '__main__':
+    a = list(yield_module_classes(classes))
+    b = a[0]
+
+    for cls in a:
+        print is_model(cls), is_abstract(cls), cls
+    pass
