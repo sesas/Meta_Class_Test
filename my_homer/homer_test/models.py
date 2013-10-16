148487255378bdf9a3189885a3fe61686918cdf2
Gabriel Reyla <gabreyla@gmail.com>
2013-10-15 18:52:54 -0700

    created sample model

--- my_homer/homer_test/models.py
+++ my_homer/homer_test/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+	name 	= models.CharField(max_length=100)
+	DOB 	= models.DatetimeField(null=True, blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+
